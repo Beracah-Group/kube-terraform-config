5d7b4d27946104d3689bb803e97e05950d2b2ce0
scotty-b <businge.scott@andela.com>
2018-06-07 12:06:11 +0300

    provision a service

--- main.tf
+++ main.tf
@@ -25,3 +25,27 @@ terraform init resource "kubernetes_pod" "echo" {
     }
   }
 }
+
+# provision a service
+resource "kubernetes_service" "echo" {
+  metadata {
+    name = "echo-example"
+  }
+
+  spec {
+    selector {
+      App = "${kubernetes_pod.echo.metadata.0.labels.App}"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
+}
