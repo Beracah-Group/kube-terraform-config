b9fd0a5ff88af827af9ad291ad9ad75504e27939
scotty-b <businge.scott@andela.com>
2018-06-07 12:03:26 +0300

    schedule simple app provider

--- main.tf
+++ main.tf
@@ -1,6 +1,27 @@
 # specify the provider
 provider "kubernetes" {}
 
-
 # Downloading plugin for provider "kubernetes"...
-terraform init
+
+# schedule a simple app
+terraform init resource "kubernetes_pod" "echo" {
+  metadata {
+    name = "echo-example"
+
+    labels {
+      App = "echo"
+    }
+  }
+
+  spec {
+    container {
+      image = "hashicorp/http-echo:0.2.1"
+      name  = "example2"
+      args  = ["-listen=:80", "-text='Hello World'"]
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
